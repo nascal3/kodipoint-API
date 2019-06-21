703b26a4ad065bad2a44018711f1760ae2649776
nascal3 <nascal3@gmail.com>
2019-06-22 02:14:56 +0300

    added default html page router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const path = require('path');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
 module.exports = router;
