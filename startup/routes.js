733e82fb339e90b3b9343e0571293d2fd9b42f60
nascal3 <nascal3@gmail.com>
2019-06-22 17:33:27 +0300

    added static webpage template

--- startup/routes.js
+++ startup/routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const users = require('../routes/users');
 
-// const home = require('../routes/home');
+const home = require('../routes/home');
 
 module.exports = (app) => {
   app.use(express.json());
   app.use('/api/users', users);
-  app.use(express.static(__dirname + 'public'));
+  // app.use('/', home);
+  app.use(express.static('public'));
 };
