0333bead136c935d8356beac4e6092f3adb9cfeb
nascal3 <nascal3@gmail.com>
2019-06-22 04:10:00 +0300

    set up users fetching end point and static home file

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Users = require('../models/userModel');
+require('express-async-errors');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:page', async (req, res) => {
+    let limit = 50;   // number of records per page
+    let offset;
+    let pageNumber = req.params.page;
+
+    const data = await Users.findAndCountAll();
+    let page = parseInt(req.params.page);      // page number
+    page <= 0 ? page = 1 : page = parseInt(req.params.page);
+    let pages = Math.ceil(data.count / limit);
+    offset = limit * (page - 1);
+
+    const users = await Users.findAll({
+        attributes: {exclude: ['Password']},
+        limit: limit,
+        offset: offset
+    });
+
+    res.status(200).json({'result': users, 'currentPage': pageNumber, 'pages': pages});
 });
 
 module.exports = router;
