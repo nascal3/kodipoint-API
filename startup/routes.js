6f7d24d9d3e93245c2972a43f673700d4e2b8c34
nascal3 <nascal3@gmail.com>
2019-06-22 17:35:17 +0300

    removed unwanted code and added web assets

--- startup/routes.js
+++ startup/routes.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const users = require('../routes/users');
 
-const home = require('../routes/home');
-
 module.exports = (app) => {
   app.use(express.json());
   app.use('/api/users', users);
-  // app.use('/', home);
   app.use(express.static('public'));
 };
