b978045fec02c4ead6724287465d482b32a1b73e
nascal3 <nascal3@gmail.com>
2020-05-27 17:44:26 +0300

    create directory before uploading image

--- helper/uploadFiles.js
+++ helper/uploadFiles.js
@@ -1,4 +1,6 @@
-module.exports = (file, data, type) => {
+const fs = require('fs');
+
+module.exports = async (file, data, type) => {
   const image = file.file;
   const timeStamp = +new Date();
 
@@ -7,8 +9,18 @@ module.exports = (file, data, type) => {
     user: 'profile'
   };
 
+  const uploadDirectory = `./uploads/images/${data.user_id}/${location[type]}`;
   const uploadPath = `./uploads/images/${data.user_id}/${location[type]}/${timeStamp}_${image.name}`;
   const displayPath = `/images/${data.user_id}/${location[type]}/${timeStamp}_${image.name}`;
+
+  if (!fs.existsSync(uploadDirectory)){
+    // fs.mkdirSync(uploadPath);
+    try {
+      await fs.promises.mkdir(uploadDirectory, { recursive: true })
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
 
   // Use the mv() method to place the file somewhere on your server
   image.mv(uploadPath, (err) => {
