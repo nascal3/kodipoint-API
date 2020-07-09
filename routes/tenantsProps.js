85aa27c0b1b14b3b2372a1ec0cc88744acbf9487
nascal3 <nascal3@gmail.com>
2020-07-09 14:18:16 +0300

    set up new endpoint to edit tenant record details

--- routes/tenantsProps.js
+++ routes/tenantsProps.js
@@ -52,27 +52,48 @@ router.post('/single', [auth], async (req, res) => {
     res.status(200).json({'result': records});
 });
 
+// GET TENANT SINGLE RENTING RECORD
+router.get('/row', [auth, landlord], async (req, res) => {
+    const recordData = await TenantsProps.findOne({
+        where: {
+            id: req.query.record_id
+        }
+    });
+
+    res.status(200).json({ 'results': recordData });
+});
+
+//**function check duplication move in entry**
+const duplicateMoveInEntry = async (propertyId, tenantId, unitNumber) => {
+    return await TenantsProps.findOne({
+        where: {
+            property_id: propertyId,
+            tenant_id: tenantId,
+            unit_no: unitNumber.toLowerCase()
+        }
+    });
+}
+
+//**function check if property being moved into is not vacant**
+const propertyNotVacant = async (propertyId, unitNumber) => {
+    return await TenantsProps.findOne({
+        where: {
+            property_id: propertyId,
+            unit_no: unitNumber.toLowerCase(),
+            move_out_date: null
+        }
+    });
+}
+
 // REGISTER TENANT TO MOVE INTO PROPERTY (add tenant to a newly rented property)
 router.post('/movein', [auth, landlord], async (req, res) => {
 
     const unitNumber = req.body.unit_no
 
-    const duplicateEntry = await TenantsProps.findOne({
-        where: {
-            property_id: req.body.property_id,
-            tenant_id: req.body.tenant_id,
-            unit_no: unitNumber.toLowerCase()
-        }
-    });
+    const duplicateEntry = await duplicateMoveInEntry(req.body.property_id, req.body.tenant_id, unitNumber)
     if (duplicateEntry) return res.status(422).json({'Error': 'The entry has already been done!'});
 
-    const propertyNotVacant = await TenantsProps.findOne({
-        where: {
-            property_id: req.body.property_id,
-            unit_no: unitNumber.toLowerCase(),
-            move_out_date: null
-        }
-    });
+    const propertyNotVacant = await  propertyNotVacant(req.body.property_id, unitNumber)
     if (propertyNotVacant) return res.status(422).json({'Error': 'This property is not vacant!'});
 
     const userData = await TenantsProps.create({
@@ -90,22 +111,43 @@ router.post('/movein', [auth, landlord], async (req, res) => {
     res.status(200).json({'result': userData});
 });
 
-// GET TENANT SINGLE RENTING RECORD
-router.get('/row', [auth, landlord], async (req, res) => {
-    const recordData = await TenantsProps.findOne({
+//**function check duplication move in entry on edit**
+const duplicateMoveInEntryEdit = async (id, propertyId, tenantId, unitNumber) => {
+    return await TenantsProps.findOne({
         where: {
-            id: req.body.record_id
+            property_id: propertyId,
+            tenant_id: tenantId,
+            unit_no: unitNumber.toLowerCase(),
+            [Op.and]: {
+                id: {
+                    [Op.ne]: id
+                }
+            }
         }
     });
+}
 
-    res.status(200).json({ 'results': recordData });
-});
+//**function check if property being moved into is not vacant on edit**
+const propertyNotVacantEdit = async (id, propertyId, unitNumber) => {
+    return await TenantsProps.findOne({
+        where: {
+            property_id: propertyId,
+            unit_no: unitNumber.toLowerCase(),
+            move_out_date: null,
+            [Op.and]: {
+                id: {
+                    [Op.ne]: id
+                }
+            }
+        }
+    });
+}
 
 // EDIT TENANT RENTING DETAILS (also used for moving tenant out of rented property)
 router.post('/edit', [auth, landlord], async (req, res) => {
 
     const editedBy = req.user.id;
-    const dbRecID = req.body.db_id;
+    const dbRecID = req.body.rec_id;
 
     const userData = await TenantsProps.findOne({
         where: {
@@ -123,6 +165,12 @@ router.post('/edit', [auth, landlord], async (req, res) => {
     const landlordName = userData.landlord_name;
     const moveInDate = userData.move_in_date;
     const moveOutDate = userData.move_out_date;
+
+    const duplicateEntry = await duplicateMoveInEntryEdit(dbRecID, req.body.property_id, req.body.tenant_id, req.body.unit_no)
+    if (duplicateEntry) return res.status(422).json({'Error': 'The entry has already been done!'});
+
+    const propertyNotVacant = await  propertyNotVacantEdit(dbRecID, req.body.property_id, req.body.unit_no)
+    if (propertyNotVacant) return res.status(422).json({'Error': 'This property is not vacant!'});
 
     const newData = await TenantsProps.update({
             tenant_id: req.body.tenant_id || tenantID,
