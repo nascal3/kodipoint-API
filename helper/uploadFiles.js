20017001abe9ee7dfc9861500f3a954746fe60d9
nascal3 <nascal3@gmail.com>
2020-09-05 12:00:02 +0300

    removed nextcloud integration code

--- helper/uploadFiles.js
+++ helper/uploadFiles.js
@@ -1,4 +1,6 @@
-const { Client, CommandStatus, UploadFilesCommand } = require('nextcloud-node-client');
+const fs = require('fs');
+const path = require('path');
+const appRoot = path.join(__dirname, '..' +'/uploads');
 
 module.exports = async (file, userID, type) => {
   const image = file.file;
@@ -9,48 +11,30 @@ module.exports = async (file, userID, type) => {
     user: 'profile'
   };
 
-  try {
-    const client = new Client();
+  const uploadDirectory = `${appRoot}/images/${userID}/${location[type]}`;
+  const uploadPath = `${appRoot}/images/${userID}/${location[type]}/${timeStamp}_${image.name}`;
+  const displayPath = `/images/${userID}/${location[type]}/${timeStamp}_${image.name}`;
 
-    // create a folder structure if not available
-    await client.createFolder(`uploads/images/${userID}/${location[type]}`);
-
-    // create a list of files to upload
-    const files = [
-      {
-        sourceFileName: image.tempFilePath,
-        targetFileName: `/uploads/images/${userID}/${location[type]}/${timeStamp}_${image.name}`
-      }
-    ];
-
-    // create the command object
-    const uc = new UploadFilesCommand(client, { files });
-
-    // start the upload asynchronously (will not throw exceptions!)
-    uc.execute();
-
-    // check the processing status as long as the command is running
-    while (uc.isFinished() !== true) {
-      // wait one second
-      await (async () => { return new Promise(resolve => setTimeout(resolve, 1000)) })();
-      console.log(uc.getPercentCompleted() + "%");
+  const createDirPath = async () => {
+    try {
+      await fs.promises.mkdir(uploadDirectory, { recursive: true });
+    } catch (err) {
+      throw (err.message);
     }
+  };
 
-    // use the result to do the needful
-    const uploadResult = uc.getResultMetaData();
+  const directoryExists = fs.access(uploadDirectory, (err) => {
+    return !err;
+  });
 
-    if (uc.getStatus() === CommandStatus.success) {
-      console.log(uploadResult.messages);
-      console.log(uc.getBytesUploaded());
-      const file = await client.getFile(`/uploads/images/${userID}/${location[type]}/${timeStamp}_${image.name}`);
-      const share = await client.createShare({ fileSystemElement: file });
-      return `${share.url}/preview`;
-    } else {
-      console.error(uploadResult.errors);
-    }
+  const movePicture = () => {
+    image.mv(uploadPath, (err) => {
+      if (err) throw (err.message);
+    });
+  };
 
-  } catch (err) {
-    throw err
-  }
+  if (!directoryExists) await createDirPath();
+  movePicture();
 
+  return displayPath
 };
